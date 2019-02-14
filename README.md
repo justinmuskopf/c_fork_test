7d313ddb7e3980a8f46f754767aa496e4d7632df
Justin Muskopf <jaymuskopf@gmail.com>
2019-02-13 19:10:22 -0600

    Add file locking

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # c_fork_test
 A simple program that tests the functionality of the fork() command in C by spawning three processes all writing to the same file.
+
+## Build Instructions
+1. execute `gcc fork_sequence_system.c`
+2. execute `./a.out [SEQUENCE_FILENAME]`
+    - where SEQUENCE_FILENAME is an optional filename of a file that contains a single number.
--- fork_sequence_system.c
+++ fork_sequence_system.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/file.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 #define MIN_RANDOM 1
 #define MAX_RANDOM 500
@@ -10,6 +13,13 @@
 #define BUFFER_LEN 4 // Max Random strlen + 1
 
 #define NUM_FORKS 3
+
+#define DEFAULT_FILENAME "sequence_num"
+
+enum {OK, ERROR};
+
+struct flock w_lock = { F_WRLCK, SEEK_SET, 0, 0, 0 };
+struct flock u_lock = { F_UNLCK, SEEK_SET, 0, 0, 0 };
 
 // Checks if a filepointer is null, and if it is,
 // Alerts user and returns true
@@ -24,18 +34,21 @@ bool fileFail(FILE *fp, char *filename)
     return false;
 }
 
+// Randomly generates an integer
+// min <= n <= max
 int randint(int min, int max)
 {
     int offset = max - min;
     if (offset < 0)
     {
-        return -1;
+        return ERROR;
     }
 
     int random = (rand() % offset) + min;
 
     return random;
 }
+
 
 // Returns the sequence number from a file, or -1 on error
 int getSequenceFromFile(char *filename)
@@ -44,13 +57,22 @@ int getSequenceFromFile(char *filename)
     FILE *fp = fopen(filename, "r");
     if (fileFail(fp, filename))
     {
-        return -1;
+        return ERROR;
     }
+
+    // File pointer's file descriptor
+    int fd = fileno(fp);
+
+    // Wait until we can acquire file lock
+    fcntl(fd, F_SETLKW, &w_lock);
 
     char buffer[BUFFER_LEN];
 
     // Read number into buffer
     fread(buffer, sizeof(char), BUFFER_LEN - 1, fp);
+
+    // Attempt to unlock file
+    fcntl(fd, F_SETLK, &u_lock);
 
     fclose(fp);
 
@@ -66,19 +88,42 @@ int writeSequenceToFile(char *filename, int sequenceNumber)
     FILE *fp = fopen(filename, "w");
     if (fileFail(fp, filename))
     {
-        return -1;
+        return ERROR;
     }
+
+    // File pointer's file descriptor
+    int fd = fileno(fp);
+
+    // Wait until we can acquire the lock
+    fcntl(fd, F_SETLKW, &w_lock);
 
     // Write sequence number to file
     ret = fprintf(fp, "%d\n", sequenceNumber);
     if (ret < 0)
     {
-        return -1;
+        return ERROR;
     }
 
+    // Attempt to unlock file
+    fcntl(fd, F_SETLK, &u_lock);
+    
     fclose(fp);
 
-    return 0;
+    return OK;
+}
+
+int generateSequenceFile(char *filename)
+{
+    int sequenceNumber = randint(MIN_RANDOM, MAX_RANDOM);
+
+    if (writeSequenceToFile(filename, sequenceNumber) == -1)
+    {
+        printf("Failed to write sequence to file!\n");
+
+        return ERROR;
+    }
+
+    return OK;
 }
 
 // Performs the child routine
@@ -97,7 +142,7 @@ void doChildRoutine(char *filename)
         if (sequenceNumber <= 0)
         {
             printf("Could not retrieve sequence number from file '%s'! Exiting child process.\n", filename);
-            exit(1);
+            exit(ERROR);
         }
 
         printf("N: %d PID: %d\n", sequenceNumber, getpid());
@@ -106,27 +151,32 @@ void doChildRoutine(char *filename)
         if (ret < 0)
         {
             printf("Failed to write sequence to file '%s'!\n", filename);
+            exit(ERROR);
         }
 
-        exit(0);
+        exit(OK);
     }
 }
 
 
 int main(int argc, char *argv[])
 {
-    // Check for proper execution
-    if (argc != 2)
-    {
-        printf("USAGE: %s filename\n", argv[0]);
-        exit(1);
-    }
-
-    // Get filename from arguments
-    char *filename = argv[1];
-
     // Seed RNG
     srand(time(NULL));
+
+    char *filename = DEFAULT_FILENAME;
+
+    if (argc != 2)
+    {
+        // Generate random sequence file
+        printf("No sequence file provided, generating a random one!\n");
+        generateSequenceFile(filename);
+    }
+    else
+    {
+        // Get filename from arguments
+        filename = argv[1];
+    }
 
     // Call fork for each child
     for (int i = 0; i < NUM_FORKS; i++)
